c8557377fadde6e470c00b0e8b6af6445325f8dd
Nicolás Servidío <nicolasservidio.dm@gmail.com>
2024-09-29 06:49:19 -0300

    Update validar_login.php
    Se incluyó script de popup window con el mensaje de "Login fallido" y un redireccionamiento posterior a index.php.
    
    Esto resuelve el bug de importación del script de head.php cuando vuelve a cargarse login.php luego del login fallido.
    
    También se interrumpió el HTTP header redirect de la función header() con un die() luego de su ejecución. No es muy relevante pero se suele seguir esta práctica.

--- validar_login.php
+++ validar_login.php
@@ -2,30 +2,35 @@
 
 if (isset($_POST['userId']) && isset($_POST['password'])) {
 
-$tbl_name="usuarios"; // Table name
+    $tbl_name="usuarios"; // Table name
 
-$username=$_POST['userId']; // username sent from form
-$password=$_POST['password']; // password sent from form
+    $username=$_POST['userId']; // username sent from form
+    $password=$_POST['password']; // password sent from form
 
-require "conn/conexion.php";
+    require "conn/conexion.php";
 
-$stmt = $conexion->prepare("SELECT * FROM $tbl_name WHERE usuario = ? AND contrasena = ?");
-$stmt->bind_param("ss", $username, $password);
+    $stmt = $conexion->prepare("SELECT * FROM $tbl_name WHERE usuario = ? AND contrasena = ?");
+    $stmt->bind_param("ss", $username, $password);
 
-$stmt->execute();
+    $stmt->execute();
 
-$result = $stmt->get_result();
+    $result = $stmt->get_result();
 
-$stmt->close();
-$conexion->close();
+    $stmt->close();
+    $conexion->close();
 
 
-if ($result->num_rows == 1) {
-    // Usuario encontrado, hacer algo aquí
-    header('location: indexGOp.php');
-} else {
-    // Usuario no encontrado
-    echo "Login fallido. Usuario o contraseña incorrectos.";
-}
+    if ($result->num_rows == 1) {
+        // Usuario encontrado, hacer algo aquí
+        header('location: indexGOp.php');
+        die();
+    } else {
+        // Usuario no encontrado
+        $popup = '<script> function loginIncorrecto() { alert("Login fallido. Usuario o contraseña incorrectos."); window.location = "index.php"; } </script>'.
+        '<script>loginIncorrecto();</script>';
+        echo $popup;
+        // echo "Login fallido. Usuario o contraseña incorrectos.";
+    }
+
 }
 ?>
