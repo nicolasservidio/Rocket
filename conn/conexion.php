47712ce65448c5655aea6f17343ebe4824dc3d06
f-mota <facumota@gmail.com>
2025-02-26 16:50:03 -0300

    Mod en conexion (Tildes)

--- conn/conexion.php
+++ conn/conexion.php
@@ -3,7 +3,7 @@
 function ConexionBD($Host = "localhost", $User = "root", $Password = "", $BaseDeDatos = "rocket") {
     
     $linkConexion = mysqli_connect($Host, $User, $Password, $BaseDeDatos,);
-
+    mysqli_set_charset($linkConexion, "utf8mb4");
     if ($linkConexion != false) {
         return $linkConexion;
     } 
